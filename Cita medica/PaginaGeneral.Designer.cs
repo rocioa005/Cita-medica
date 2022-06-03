f3f7df7b482b03da8b8b5d69df040196610e9dd3
rocioa005 <106789755+rocioa005@users.noreply.github.com>
2022-06-02 22:35:30 -0400

    color

--- Cita medica/PaginaGeneral.Designer.cs
+++ Cita medica/PaginaGeneral.Designer.cs
@@ -28,10 +28,18 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // PaginaGeneral
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Name = "PaginaGeneral";
             this.Text = "PaginaGeneral";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
